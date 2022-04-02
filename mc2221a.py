68e18d851f565507632e514e65e3ff3b6f5b19a3
nickGermi <nick@emz.com.au>
2022-04-03 01:34:34 +1100

    version 0.2
    Better comments
    Easier to configure
    Cleaner functions

--- mc2221a.py
+++ mc2221a.py
@@ -1,17 +1,34 @@
 import os
+# board needs this
 os.environ['BLINKA_MCP2221'] = "1"
+# if can't connect to board, change this value to 0.5 or higher
 os.environ['BLINKA_MCP2221_RESET_DELAY'] = "-1"
+
+# Define your preferred range here
+# if lux within range of 0-25 increament by 1 then set to 10% brightness
+# average_lux_count is amount of seconds to hold lux values
+config = {
+    "lux_range": {
+        range(0, 25, 1): 10,
+        range(25, 50, 1): 20,
+        range(50, 100, 1): 30,
+        range(100, 150, 1): 40,
+        range(150, 200, 1): 50,
+        range(200, 250, 1): 60,
+        range(250, 300, 1): 70,
+        range(300, 350, 1): 80,
+        range(350, 400, 1): 90,
+        range(400, 5500, 1): 100
+    },
+    "average_lux_count": 15
+}
 
 import threading
 import board
 import adafruit_tsl2591
 import time
+from datetime import datetime
 import screen_brightness_control
-
-values = []
-cb = 0
-average_lux = 0
-sensor = object
 
 def connect_sensor():
     global sensor
@@ -28,7 +45,7 @@ def generate_value_array(delay, run_event, sensor):
         lux = sensor.lux
         if isinstance(lux, float):
             if lux > 0:
-                if (len(values) > 9 ):
+                if (len(values) >= config["average_lux_count"]):
                     values.pop(0)
                 values.append(int(lux))
         time.sleep(delay)
@@ -36,94 +53,59 @@ def generate_value_array(delay, run_event, sensor):
 def average(lst):
     return int(round(sum(lst) / len(lst)))
 
+# Gets brightness of primary monitor (display=0), to set to a different monitor
+# use screen_brightness_control.get_brightness(display=1)[0]
+# refer to https://pypi.org/project/screen-brightness-control/
 def get_brightness():
     return screen_brightness_control.get_brightness()[0]
 
+# Sets brightness of primary monitor, to set to a different monitor
+# use screen_brightness_control.set_brightness(brvalue, display=1)
+# refer to https://pypi.org/project/screen-brightness-control/
 def set_brightness(brvalue):
     global cb
     screen_brightness_control.set_brightness(brvalue)
     cb = brvalue
 
-def calculate():
-    global cb
-    global average_lux
-    cbstr = str(cb)+"%"
-    CSI = "\x1B["
-    print(CSI+"37;40m" + "Brightness is", cbstr, "and Lux is", average_lux, "        " + CSI + "0m", end = "\r")
-    if (average_lux in range(0, 25, 1)):
-        if (cb != 10):
-            set_brightness(10)
-        return
-    if (average_lux in range(25, 50, 1)):
-        if (cb != 20):
-            set_brightness(20)
-        return
-    if (average_lux in range(50, 100, 1)):
-        if (cb != 30):
-            set_brightness(30)
-        return
-    if (average_lux in range(100, 150, 1)):
-        if (cb != 40):
-            set_brightness(40)
-        return
-    if (average_lux in range(150, 200, 1)):
-        if (cb != 50):
-            set_brightness(50)
-        return
-    if (average_lux in range(200, 250, 1)):
-        if (cb != 60):
-            set_brightness(60)
-        return
-    if (average_lux in range(250, 300, 1)):
-        if (cb != 70):
-            set_brightness(70)
-        return
-    if (average_lux in range(300, 350, 1)):
-        if (cb != 80):
-            set_brightness(80)
-        return
-    if (average_lux in range(350, 400, 1)):
-        if (cb != 90):
-            set_brightness(90)
-        return
-    if (average_lux in range(400, 5500, 1)):
-        if (cb != 100):
-            set_brightness(100)
-        return
-    else:
-        print("lux is not in range!")
+def switcher(argument):
+    for key in config["lux_range"]:
+        if type(key) is range and argument in key and cb != config["lux_range"][key]:
+            print(CSI + "90;40m " + datetime.today().strftime("%I:%M%p") + CSI + "0m Last {0} seconds average Lux is {1} setting brightness to {2}%".format(config["average_lux_count"], argument, config["lux_range"][key]))
+            set_brightness(config["lux_range"][key])
 
 if __name__ == '__main__':
     os.system('cls')
     CSI = "\x1B["
-    scriptheader = """      ___                       ___     
-     /__/\        ___          /__/|    
-     \  \:\      /  /\        |  |:|    
-      \  \:\    /  /:/        |  |:|    
-  _____\__\:\  /__/::\      __|__|:|    
- /__/::::::::\ \__\/\:\__  /__/::::\____
- \  \:\~~\~~\/    \  \:\/\    ~\~~\::::/
-  \  \:\  ~~~      \__\::/     |~~|:|~~ 
-   \  \:\          /__/:/      |  |:|   
-    \  \:\         \__\/       |  |:|   
-     \__\/                     |__|/    """
+    scriptheader = """
+ ┌────────────────────────────────────────┐
+ │     $$\   $$\ $$$$$$\ $$\   $$\        │
+ │     $$$\  $$ |\_$$  _|$$ |  $$ |       │
+ │     $$$$\ $$ |  $$ |  \$$\ $$  |       │
+ │     $$ $$\$$ |  $$ |   \$$$$  /        │
+ │     $$ \$$$$ |  $$ |   $$  $$<         │
+ │     $$ |\$$$ |  $$ |  $$  /\$$\        │
+ │     $$ | \$$ |$$$$$$\ $$ /  $$ |       │
+ │     \__|  \__|\______|\__|  \__|       │
+ ├────────────────────────────────────────┤
+ │  Monitor Brightness Manager v0.2       │
+ │  Light sensor: TSL2591                 │
+ │  USB 2.0 to I2C converter: MCP2221A    │
+ └────────────────────────────────────────┘"""
     print(CSI + "33;40m" + scriptheader + CSI + "0m")
-    print(CSI + "90;40m" + "=========================================" + CSI + "0m")
-    print(CSI + "90;40m" + "==  Auto DDC/CI brightness manager     ==" + CSI + "0m")
-    print(CSI + "90;40m" + "==  Light sensor: TSL2591              ==" + CSI + "0m")
-    print(CSI + "90;40m" + "==  Controller chip: MCP2221A          ==" + CSI + "0m")
-    print(CSI + "90;40m" + "=========================================" + CSI + "0m")
+    values = []
+    cb = 0
+    average_lux = 0
     sensor = connect_sensor()
     run_event = threading.Event()
     run_event.set()
     sensor_values = threading.Thread(target=generate_value_array, args=(0.5, run_event, sensor))
     sensor_values.start()
     cb = get_brightness()
+    print(CSI + "90;40m " + datetime.today().strftime("%I:%M%p") + CSI + "0m Current brightness is set to {0}% and average lux is {1}.".format(cb, average(values)))
     try:
         while True:
             if (len(values) > 0):
-                average_lux = average(values)
-                calculate()
+                switcher(average(values))
             time.sleep(1)
     except KeyboardInterrupt:
         print("\nAttempting to close threads..")
