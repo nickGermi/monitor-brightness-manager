5642a47d9fb2b29b3a2b76b5d28e8110fed92b36
nickGermi <nick@emz.com.au>
2022-04-06 23:55:20 +1000

    v0.5

--- monitor_brightness_manager.py
+++ monitor_brightness_manager.py
@@ -1,12 +1,11 @@
-import os, threading, time, datetime, screen_brightness_control, sys
-
 ###################
 ## CONFIGURATION ##
 ###################
 # BLINKA_MCP2221 needs to be 1
 # BLINKA_MCP2221_RESET_DELAY value should be -1 in Windows 11, you can try 0.5 or higher if getting errors
-# Define your preferred range here (lux_start, lux_end, increament_by): brightness
+# lux_range is your preferred brightness vs ambient light config, (lux_start, lux_end, increament_by): brightness
 # "average_lux_count" is amount of seconds to hold lux values before taking average
+# display_id is your monitor's ID, primary monitor is 0 and is set by default
 config = {
     "BLINKA_MCP2221": "1",
     "BLINKA_MCP2221_RESET_DELAY": "-1",
@@ -22,15 +21,18 @@ config = {
         range(350, 400, 1): 90,
         range(400, 5500, 1): 100
     },
-    "average_lux_count": 10
+    "average_lux_count": 10,
+    "display_id": 0
 }
 ################
 ## END CONFIG ##
 ################
 
+import os, threading, time, datetime, screen_brightness_control, sys
+
 def getscript(script):
     if(initial_run):
-        return script.replace('\\','\\\\')
+        return script.replace("\\","\\\\")
     else:
         return script
 
@@ -47,7 +49,7 @@ def connect_device():
         time.sleep(1)
         return board.I2C()
     except Exception as e:
-        print('exception during connect_device..', e)
+        print("exception during connect_device..", e)
         restart(5)
 
 def connect_sensor():
@@ -55,98 +57,77 @@ def connect_sensor():
         import adafruit_tsl2591
         return adafruit_tsl2591
     except Exception as e:
-        print('exception during connect_sensor..', e)
+        print("exception during connect_sensor..", e)
         restart(5)
 
-def get_sensor_values():
+def get_sensor_value():
     board_i2c = connect_device()
     sensor = connect_sensor()
     try:
         sensor_values = sensor.TSL2591(board_i2c)
         return sensor_values
     except Exception as e:
-        print('exception during get_sensor_values..', e)
+        print("exception during get_sensor_value..", e)
         restart(5)
 
 def generate_value_array(delay, run_event, sensor):
     while run_event.is_set():
         try:
-            if sensor.lux:
-                if isinstance(sensor.lux, float):
-                    if sensor.lux > 0:
-                        if (len(values) >= config["average_lux_count"]):
-                            values.pop(0)
-                        values.append(int(sensor.lux))
-                        time.sleep(delay)
+            values.pop(0)
+            values.append(int(float(sensor.lux)))
+            time.sleep(delay)
         except Exception as e:
-            print('exception during generate_value_array..', e)
+            print("exception during generate_value_array..", e)
             restart(5)
-
 
 def average(lst):
     return int(round(sum(lst) / len(lst)))
 
-# Gets brightness of primary monitor (display=0), to set to a different monitor
-# use screen_brightness_control.get_brightness(display=1)[0]
 def get_brightness():
     try:
-        return screen_brightness_control.get_brightness()[0]
+        return screen_brightness_control.get_brightness(display=config["display_id"])[0]
     except Exception as e:
-        print('exception during get_brightness..', e)
-        print('re-trying in 1 second')
+        print("exception during get_brightness..", e)
+        print("re-trying in 1 second")
         time.sleep(1)
         get_brightness()
 
-# Sets brightness of primary monitor, to set to a different monitor
-# use screen_brightness_control.set_brightness(brvalue, display=1)
 def set_brightness(brvalue):
     global cb
     try:
-        screen_brightness_control.set_brightness(brvalue)
+        screen_brightness_control.fade_brightness(brvalue, cb, display=config["display_id"], blocking=False)
         cb = brvalue
     except Exception as e:
-        print('exception during set_brightness..', e)
-        print('re-trying in 1 second')
+        print("exception during set_brightness..", e)
+        print("re-trying in 1 second")
         time.sleep(1)
         set_brightness(brvalue)
+
+def print_status(argument):
+    print("\x1B[33;40m" + datetime.datetime.today().strftime("%I:%M:%S%p") + "\x1B[0m Average Lux is {0}, brightness is set to {1}%.".format(argument[0], argument[1]))
 
 def switcher(argument):
     for key in config["lux_range"]:
         if type(key) is range and argument in key and cb != config["lux_range"][key]:
-            print(CSI + "33;40m" + datetime.datetime.today().strftime("%H:%M:%S%p") + CSI + "0m Last {0} seconds average Lux is {1} setting brightness to {2}%".format(config["average_lux_count"], argument, config["lux_range"][key]))
+            print_status([argument, config["lux_range"][key]])
             set_brightness(config["lux_range"][key])
 
-def print_initial_status(args):
-    if(args[0] and args[1] > 0):
-        print(CSI + "33;40m" + datetime.datetime.today().strftime("%H:%M:%S%p") + CSI + "0m Current brightness is set to {0}% and current lux is {1}.".format(args[0], int(round(float(args[1])))))
-    else:
-        print('could not get initial value, re-trying in 1 second')
-        time.sleep(1)
-        print_initial_status(args)
-
-if __name__ == '__main__':
-    os.system('cls')
+if __name__ == "__main__":
+    os.system("cls")
     initial_run = 1
     script = sys.argv[0]
-    os.environ['BLINKA_MCP2221'] = config["BLINKA_MCP2221"]
-    os.environ['BLINKA_MCP2221_RESET_DELAY'] = config["BLINKA_MCP2221_RESET_DELAY"]
-    CSI = "\x1B["
-    scriptheader = """┌────────────────────────────────────────┐
-│  Monitor Brightness Manager v0.4       │
-│  Light sensor: TSL2591                 │
-│  USB 2.0 to I2C converter: MCP2221A    │
-└────────────────────────────────────────┘"""
-    print(CSI + "33;40m" + scriptheader + CSI + "0m")
-    values = []
-    cb = 0
-    average_lux = 0
-    sensor = get_sensor_values()
+    os.environ["BLINKA_MCP2221"] = config["BLINKA_MCP2221"]
+    os.environ["BLINKA_MCP2221_RESET_DELAY"] = config["BLINKA_MCP2221_RESET_DELAY"]
+    print("\x1B[33;40mMonitor Brightness Manager v0.5\nLight sensor: TSL2591\nI2C converter: MCP2221A\n\n\x1B[0m")
+    cb = get_brightness()
+    sensor = get_sensor_value()
+    lux = int(float(sensor.lux))
+    values = [lux] * config["average_lux_count"]
     run_event = threading.Event()
     run_event.set()
     sensor_values = threading.Thread(target=generate_value_array, args=(1, run_event, sensor))
     sensor_values.start()
-    cb = get_brightness()
-    print_initial_status([cb, sensor.lux])
+    print_status([lux, cb])
     try:
         while True:
             if (len(values) > 0):
